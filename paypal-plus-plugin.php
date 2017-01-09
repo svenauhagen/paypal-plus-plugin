0a554c39e5c63eb5f4188cc4a5365a6b87a58c1c
Moritz Meißelbach <m.meisselbach@inpsyde.com>
2017-01-09 10:38:38 +0100

    Add basic l10n setup

--- paypal-plus-plugin.php
+++ paypal-plus-plugin.php
@@ -2,12 +2,12 @@
 /**
  * Plugin Name: PayPal Plus Plugin
  * Description: Official WordPress plugin for PayPal Plus
- * Plugin URI:  TODO
  * Author:      Inpsyde GmbH
  * Author URI:  http://inpsyde.com/
  * Version:     dev-master
  * License:     MIT
  * Text Domain: paypal-plus-plugin
+ * Domain Path: /languages/
  */
 
 namespace PayPalPlusPlugin;
