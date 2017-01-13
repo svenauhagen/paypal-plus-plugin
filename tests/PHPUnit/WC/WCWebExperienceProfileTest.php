42940741f90266e5ef37068120e5201fab8991e4
Moritz Meißelbach <m.meisselbach@inpsyde.com>
2017-01-13 14:15:18 +0100

    Add empty test class

---
+++ tests/PHPUnit/WC/WCWebExperienceProfileTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: biont
+ * Date: 13.01.17
+ * Time: 12:50
+ */
+
+namespace PayPalPlusPlugin\WC;
+
+use MonkeryTestCase\BrainMonkeyWpTestCase;
+
+class WCWebExperienceProfileTest extends BrainMonkeyWpTestCase {
+	/**
+	 * @return array
+	 */
+	public function default_test_data() {
+
+		$data = [];
+
+		# 1. Testrun
+		$data['test_1'] = [
+
+		];
+
+		# 2. Testrun
+		$data['test_2'] = [
+
+		];
+
+		# 3. Testrun
+		$data['test_2'] = [
+
+		];
+
+		return $data;
+	}
+
+}
