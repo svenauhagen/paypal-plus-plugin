b41d28171705658fcca754368a55996eeaf82002
Moritz Meißelbach <m.meisselbach@inpsyde.com>
2017-02-06 16:31:44 +0100

    Much work on price/tax handling

--- tests/PHPUnit/WC/CredentialVerificationTest.php
+++ tests/PHPUnit/WC/CredentialVerificationTest.php
@@ -17,54 +17,54 @@ class CredentialVerificationTest extends BrainMonkeyWpTestCase {
 	/**
 	 * @runInSeparateProcess
 	 */
-	//public function test_verify() {
-	//
-	//	return;
-	//	$paymentMock = \Mockery::mock( 'alias:' . 'PayPal\Api\Payment' );
-	//	$paymentMock->shouldReceive( 'all' )
-	//	            ->once();
-	//
-	//	$credentialMock = \Mockery::mock( OAuthTokenCredential::class );
-	//	$credentialMock->shouldReceive( 'getClientId' )
-	//	               ->andReturn( 'fhlwdjakfhwi' );
-	//	$credentialMock->shouldReceive( 'getClientSecret' )
-	//	               ->andReturn( 'jaajvhsulvsj' );
-	//
-	//	$context = \Mockery::mock( ApiContext::class );
-	//	$context->shouldReceive( 'getCredential' )
-	//	        ->once()
-	//	        ->andReturn( $credentialMock );
-	//
-	//	$testee = new CredentialVerification( $context );
-	//	$result = $testee->verify();
-	//	$this->assertTrue( $result );
-	//
-	//}
+	public function test_verify() {
+
+		return;
+		$paymentMock = \Mockery::mock( 'alias:' . 'PayPal\Api\Payment' );
+		$paymentMock->shouldReceive( 'all' )
+		            ->once();
+
+		$credentialMock = \Mockery::mock( OAuthTokenCredential::class );
+		$credentialMock->shouldReceive( 'getClientId' )
+		               ->andReturn( 'fhlwdjakfhwi' );
+		$credentialMock->shouldReceive( 'getClientSecret' )
+		               ->andReturn( 'jaajvhsulvsj' );
+
+		$context = \Mockery::mock( ApiContext::class );
+		$context->shouldReceive( 'getCredential' )
+		        ->once()
+		        ->andReturn( $credentialMock );
+
+		$testee = new CredentialVerification( $context );
+		$result = $testee->verify();
+		$this->assertTrue( $result );
+
+	}
 
 	/**
 	 * @runInSeparateProcess
 	 */
-	//public function test_verify_no_credentials() {
-	//
-	//	return;
-	//	$credentialMock = \Mockery::mock( OAuthTokenCredential::class );
-	//	$credentialMock->shouldReceive( 'getClientId' )
-	//	               ->andReturn( '' );
-	//	$credentialMock->shouldReceive( 'getClientSecret' )
-	//	               ->andReturn( '' );
-	//
-	//	$context = \Mockery::mock( ApiContext::class );
-	//	$context->shouldReceive( 'getCredential' )
-	//	        ->once()
-	//	        ->andReturn( $credentialMock );
-	//
-	//	$testee = new CredentialVerification( $context );
-	//	$result = $testee->verify();
-	//	$this->assertFalse( $result );
-	//
-	//	$errors = $testee->get_error_message();
-	//	$this->assertInternalType( 'string', $errors );
-	//	$this->assertNotEmpty( $errors );
-	//}
+	public function test_verify_no_credentials() {
+
+		return;
+		$credentialMock = \Mockery::mock( OAuthTokenCredential::class );
+		$credentialMock->shouldReceive( 'getClientId' )
+		               ->andReturn( '' );
+		$credentialMock->shouldReceive( 'getClientSecret' )
+		               ->andReturn( '' );
+
+		$context = \Mockery::mock( ApiContext::class );
+		$context->shouldReceive( 'getCredential' )
+		        ->once()
+		        ->andReturn( $credentialMock );
+
+		$testee = new CredentialVerification( $context );
+		$result = $testee->verify();
+		$this->assertFalse( $result );
+
+		$errors = $testee->get_error_message();
+		$this->assertInternalType( 'string', $errors );
+		$this->assertNotEmpty( $errors );
+	}
 
 }
