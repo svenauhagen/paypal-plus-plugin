55a80cdaf727743cff5141f6880f25cf422917e7
Moritz Meißelbach <m.meisselbach@inpsyde.com>
2017-03-15 10:09:33 +0100

    Show PUI details on view-order.php #PPP-6

--- src/WC/PUI/PaymentInstructionRenderer.php
+++ src/WC/PUI/PaymentInstructionRenderer.php
@@ -22,6 +22,7 @@ class PaymentInstructionRenderer {
 
 		add_action( 'woocommerce_thankyou_paypal_plus', [ $this, 'delegate_thankyou' ], 10, 1 );
 		add_action( 'woocommerce_email_before_order_table', [ $this, 'delegate_email' ], 10, 3 );
+		add_action( 'woocommerce_view_order', [ $this, 'delegate_view_order' ], 10, 1 );
 	}
 
 	/**
@@ -44,9 +45,9 @@ class PaymentInstructionRenderer {
 	/**
 	 * Gather needed data and then render the view, if possible
 	 *
-	 * @param \WC_Order $order WooCommerce order.
-	 * @param bool      $sent_to_admin
-	 * @param bool      $plain_text
+	 * @param \WC_Order $order         WooCommerce order.
+	 * @param bool      $sent_to_admin Will the eMail be sent to the site admin?.
+	 * @param bool      $plain_text    Should we render as plain text?.
 	 */
 	public function delegate_email( \WC_Order $order, $sent_to_admin, $plain_text = false ) {
 
@@ -56,6 +57,23 @@ class PaymentInstructionRenderer {
 		}
 		$pui_view = new PaymentInstructionView( $pui_data );
 		$pui_view->email_instructions();
+	}
+
+	/**
+	 * Gather needed data and then render the view, if possible.
+	 *
+	 * @param int $order_id WooCommerce order ID.
+	 */
+	public function delegate_view_order( $order_id ) {
+
+		$order    = wc_get_order( $order_id );
+		$pui_data = new PaymentInstructionData( $order );
+		if ( ! $pui_data->has_payment_instructions() ) {
+			return;
+		}
+		$pui_view = new PaymentInstructionView( $pui_data );
+		$pui_view->thankyou_page();
+
 	}
 
 }
