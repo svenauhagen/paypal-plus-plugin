efb92c4c5b6af2297d30773b817f45f36c52713f
Christina Fuchs <christina.fuchs@vanvox.de>
2017-10-16 14:19:17 +0200

    add WC requires at least and WC tested up to #PPP-116
    set plugin version to 1.0.5

--- paypalplus-woocommerce.php
+++ paypalplus-woocommerce.php
@@ -4,7 +4,9 @@
  * Description: PayPal Plus - the official WordPress Plugin for WooCommerce
  * Author:      Inpsyde GmbH
  * Author URI:  https://inpsyde.com/
- * Version:     1.0.4
+ * Version:     1.0.5
+ * WC requires at least: 3.0.0
+ * WC tested up to: 3.2.1
  * License:     MIT
  * Text Domain: woo-paypalplus
  * Domain Path: /languages/
