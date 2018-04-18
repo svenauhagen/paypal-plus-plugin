af0950e87c96d0139b09eac00b528d7b953da974
Christina Fuchs <c.fuchs@inpsyde.com>
2018-04-18 09:10:39 +0000

    set version to 1.0.7

--- paypalplus-woocommerce.php
+++ paypalplus-woocommerce.php
@@ -4,7 +4,7 @@
  * Description: PayPal Plus - the official WordPress Plugin for WooCommerce
  * Author:      Inpsyde GmbH
  * Author URI:  https://inpsyde.com/
- * Version:     1.0.6
+ * Version:     1.0.7
  * WC requires at least: 3.0.0
  * WC tested up to: 3.3.5
  * License:     MIT
