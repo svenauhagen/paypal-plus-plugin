0391c8e8371bb92a6d6667ca4560d96257c50960
Guido Scialfa <dev@guidoscialfa.com>
2019-03-21 10:43:59 +0100

    bump 2.0.0.3

--- paypalplus-woocommerce.php
+++ paypalplus-woocommerce.php
@@ -6,7 +6,7 @@
  * Description: PayPal Plus - the official WordPress Plugin for WooCommerce
  * Author:      Inpsyde GmbH
  * Author URI:  https://inpsyde.com/
- * Version:     2.0.0.0
+ * Version:     2.0.0.3
  * WC requires at least: 3.0.0
  * WC tested up to: 3.5.5
  * License:     MIT
