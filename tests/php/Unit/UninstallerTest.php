4845a846331f2ae2d981cf8bcf74b71ff91b1390
Guido Scialfa <dev@guidoscialfa.com>
2019-07-17 12:52:05 +0200

    Move unit tests under the appropiated directory
    PPP-374

---
+++ tests/php/Unit/UninstallerTest.php
@@ -0,0 +1,23 @@
+<?php # -*- coding: utf-8 -*-
+/*
+ * This file is part of the PayPal PLUS for WooCommerce package.
+ *
+ * (c) Inpsyde GmbH
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WCPayPalPlus\Tests\Unit\Uninstall;
+
+use WCPayPalPlus\Uninstall\Uninstaller;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class UninstallerTest
+ * @package WCPayPalPlus\Tests\Unit\Uninstall
+ */
+class UninstallerTest extends TestCase
+{
+
+}
