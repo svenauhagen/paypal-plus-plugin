e02eb481d9f4f346a10740481c1e871fffd3136d
Guido Scialfa <dev@guidoscialfa.com>
2019-11-12 14:34:24 +0100

    Render Express Checkout Payments Button on `updated_shipping_method` event
    There are more places where the express checkout button is printed and
    depending on the place WooCommerce could make ajax calls which change
    the markup structure.
    
    When that happen the payment button is removed from the DOM this because
    there's no more a reference to an element in the dom.
    
    When then the markup is recreated PPP does not trigger the render of the
    button.
    
    There is a list of events in the `expressCheckout` that I ported into
    php env to allow third parties to filter that list in this way possible
    renders needed because of other WooCommerce parts or other plugins can be
    addressed at support level, than we can add those in a future release.
    
    Basically will not block the merchant to have a functional button in case
    of cases we haven't considered yet.
    
    PPP-386

--- src/Assets/AssetManager.php
+++ src/Assets/AssetManager.php
@@ -17,6 +17,8 @@ class AssetManager
 {
     use AssetManagerTrait;
 
+    const FILTER_EXPRESS_CHECKOUT_JS_DATA = 'paypalplus.express_checkout_data';
+
     /**
      * @var PluginProperties
      */
@@ -185,7 +187,23 @@ class AssetManager
                 'action' => AjaxHandler::ACTION,
                 'ajaxUrl' => home_url('/wp-admin/admin-ajax.php'),
             ],
+            'paymentButtonRenderEvents' => [
+                'wc_fragments_refreshed',
+                'wc_fragments_loaded',
+                'removed_from_cart',
+                'added_to_cart',
+                'updated_shipping_method',
+            ],
         ];
+
+        /**
+         * Filter Payment Button Render Events
+         *
+         * The client events for which the button has to be re rendered
+         *
+         * @param array $paymentButtonRenderEvents The list of the events
+         */
+        $data = apply_filters(self::FILTER_EXPRESS_CHECKOUT_JS_DATA, $data);
 
         /** @noinspection AdditionOperationOnArraysInspection */
         return $data + $this->smartButtonArguments->toArray();
