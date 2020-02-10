2067f6baf3643de49f804a5072277fc1a1acbd79
Guido Scialfa <dev@guidoscialfa.com>
2020-02-10 14:36:25 +0100

    Update tested up to WooCommerce 3.9.1

--- paypalplus-woocommerce.php
+++ paypalplus-woocommerce.php
@@ -8,7 +8,7 @@
  * Author URI: https://inpsyde.com/
  * Version: 2.1.1
  * WC requires at least: 3.6.4
- * WC tested up to: 3.9.0
+ * WC tested up to: 3.9.1
  * License: GPLv2+
  * Text Domain: woo-paypalplus
  * Domain Path: /languages/
