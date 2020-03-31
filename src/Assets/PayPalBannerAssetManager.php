f92ddd91207932480748c82b9acd068b583d4688
carmenmaymo <mayen22@gmail.com>
2020-03-31 11:57:03 +0200

    Refactor and tests added
    PPP-413
    Test to check there's no enqueue when feature is disabled. Refactor to
    improve testability. Added doc-blocks to improve collaboration.

--- src/Assets/PayPalBannerAssetManager.php
+++ src/Assets/PayPalBannerAssetManager.php
@@ -32,7 +32,10 @@ class PayPalBannerAssetManager
         $this->sharedRepository = $sharedRepository;
     }
 
-    public function enqueuePPBannerFrontEndScripts()
+    /**
+     * Register all the scripts related to Banner
+     */
+    public function registerScripts()
     {
         list($assetPath, $assetUrl) = $this->assetUrlPath();
         wp_register_script(
@@ -42,77 +45,129 @@ class PayPalBannerAssetManager
             filemtime("{$assetPath}/public/js/paypalBanner.min.js"),
             true
         );
-        if (!$this->isEnabledShowBannerInPage('banner_settings_enableBanner')) {
-            return;
-        }
-        if (!$this->isAllowedContext($this->bannerSettings())) {
-            return;
-        }
-        $this->conditionallyEnqueueScript();
     }
 
-    protected function isAllowedContext(array $settings)
+    /**
+     * @deprecated
+     * @see enqueueFrontEndScripts
+     */
+    public function enqueuePPBannerFrontEndScripts()
     {
-        return $this->isBannerEnabledWCContext($settings['optional_pages']);
+        $this->enqueueFrontEndScripts();
     }
 
-    protected function bannerSettings()
+    /**
+     * Enqueues the Banner frontend scripts
+     * if conditions apply
+     */
+    public function enqueueFrontEndScripts()
     {
-        $scriptUrl = $this->paypalScriptUrl();
-        $amount = $this->calculateAmount();
+        if (!$this->isEnqueueAllowed()) {
+            return;
+        }
 
-        $settings = [
-            'amount' => $amount,
-            'script_url' => $scriptUrl,
-            'enabled_banner' => $this->isEnabledShowBannerInPage(
-                'banner_settings_enableBanner'
+        $this->enqueueScripts();
+    }
+
+    /**
+     * Returns false if the banner feature is disabled
+     * Returns false if in a page where feature is disabled
+     * @return bool
+     */
+    protected function isEnqueueAllowed()
+    {
+        if (!$this->isEnabledOption('banner_settings_enableBanner')) {
+            return false;
+        }
+        if (!$this->isAllowedContext($this->optionalPagesSetting())) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @param string $option  The option associated with the page to check
+     *
+     * @return bool Returns the option as a boolean
+     */
+    protected function isEnabledOption($option)
+    {
+        return wc_string_to_bool(
+            get_option($option, 'no')
+        );
+    }
+
+    /**
+     * Check if in a page where the banner is enabled
+     *
+     * @param array $settings The optional pages setting
+     *
+     * @return bool True if we are in a page where the settings is enabled
+     */
+    protected function isAllowedContext($settings)
+    {
+        return (is_home() && isset($settings['show_home'])
+                ? $settings['show_home'] : false)
+            || (is_shop() && isset($settings['show_category'])
+                ? $settings['show_category'] : false)
+            || (is_search() && isset($settings['show_search'])
+                ? $settings['show_search'] : false)
+            || (is_product() && isset($settings['show_product'])
+                ? $settings['show_product'] : false)
+            || (is_cart() && isset($settings['show_cart'])
+                ? $settings['show_cart'] : false)
+            || (is_checkout() && isset($settings['show_checkout'])
+                ? $settings['show_checkout'] : false);
+    }
+
+    /**
+     * Gets the option values for the optional pages.
+     * @return array
+     */
+    protected function optionalPagesSetting()
+    {
+        return [
+            'show_home' => $this->isEnabledOption(
+                'banner_settings_home'
             ),
-            'optional_pages' => [
-                'show_home' => $this->isEnabledShowBannerInPage(
-                    'banner_settings_home'
-                ),
-                'show_category' => $this->isEnabledShowBannerInPage(
-                    'banner_settings_products'
-                ),
-                'show_search' => $this->isEnabledShowBannerInPage(
-                    'banner_settings_search'
-                ),
-                'show_product' => $this->isEnabledShowBannerInPage(
-                    'banner_settings_product_detail'
-                ),
-                'show_cart' => $this->isEnabledShowBannerInPage(
-                    'banner_settings_cart'
-                ),
-                'show_checkout' => $this->isEnabledShowBannerInPage(
-                    'banner_settings_checkout'
-                ),
-            ],
-            'style' => [
-                'layout' => get_option('banner_settings_layout'),
-                'logo' => [
-                    'type' => get_option('banner_settings_textSize'),
-                    'color' => get_option('banner_settings_textColor'),
-                ],
-                'color' => get_option('banner_settings_flexColor'),
-                'ratio' => get_option('banner_settings_flexSize'),
-            ],
+            'show_category' => $this->isEnabledOption(
+                'banner_settings_products'
+            ),
+            'show_search' => $this->isEnabledOption(
+                'banner_settings_search'
+            ),
+            'show_product' => $this->isEnabledOption(
+                'banner_settings_product_detail'
+            ),
+            'show_cart' => $this->isEnabledOption(
+                'banner_settings_cart'
+            ),
+            'show_checkout' => $this->isEnabledOption(
+                'banner_settings_checkout'
+            ),
         ];
-
-        return $settings;
     }
 
-    protected function conditionallyEnqueueScript()
+    /**
+     * Enqueues the scripts to the footer
+     * and adds the markup to show on page.
+     */
+    protected function enqueueScripts()
     {
         add_action(
             'wp_footer',
             function () {
-                $this->showBanner();
+                $this->bannerFrontEndScript();
             }
         );
         $this->placeBannerOnPage();
     }
 
-    protected function showBanner()
+    /**
+     * Enqueues the front end script for the banner feature
+     * loads settings data
+     */
+    protected function bannerFrontEndScript()
     {
         $settings = $this->bannerSettings();
         list($assetPath, $assetUrl) = $this->assetUrlPath();
@@ -132,35 +187,38 @@ class PayPalBannerAssetManager
         );
     }
 
-    protected function isBannerEnabledWCContext($settings)
+    /**
+     * Retrieves the settings data
+     * related to the banner feature
+     * @return array
+     */
+    protected function bannerSettings()
     {
-        return (is_home() && isset($settings['show_home'])
-                ? $settings['show_home'] : false)
-            || (is_shop() && isset($settings['show_category'])
-                ? $settings['show_category'] : false)
-            || (is_search() && isset($settings['show_search'])
-                ? $settings['show_search'] : false)
-            || (is_product() && isset($settings['show_product'])
-                ? $settings['show_product'] : false)
-            || (is_cart() && isset($settings['show_cart'])
-                ? $settings['show_cart'] : false)
-            || (is_checkout() && isset($settings['show_checkout'])
-                ? $settings['show_checkout'] : false);
+        $scriptUrl = $this->paypalScriptUrl();
+        $amount = $this->calculateAmount();
+
+        return [
+            'amount' => $amount,
+            'script_url' => $scriptUrl,
+            'enabled_banner' => $this->isEnabledOption(
+                'banner_settings_enableBanner'
+            ),
+            'optional_pages' => $this->optionalPagesSetting(),
+            'style' => [
+                'layout' => get_option('banner_settings_layout'),
+                'logo' => [
+                    'type' => get_option('banner_settings_textSize'),
+                    'color' => get_option('banner_settings_textColor'),
+                ],
+                'color' => get_option('banner_settings_flexColor'),
+                'ratio' => get_option('banner_settings_flexSize'),
+            ],
+        ];
     }
 
-    protected function calculateAmount()
-    {
-        wc_load_cart();
-
-        $amount = WC()->cart->get_total('edit');
-
-        if (is_product() && is_numeric(wc_get_product()->get_price('edit'))) {
-            return $amount + wc_get_product()->get_price('edit');
-        }
-
-        return $amount;
-    }
-
+    /**
+     * @return string The script url with clientID and currency
+     */
     protected function paypalScriptUrl()
     {
         $clientId = get_option('banner_settings_clientID');
@@ -176,6 +234,25 @@ class PayPalBannerAssetManager
         return "https://www.paypal.com/sdk/js?client-id={$clientId}&components=messages&currency={$currency}";
     }
 
+    /**
+     * Retrieves the cart amount and adds it to the product price
+     * if we are in a product page.
+     * @return float The total amount
+     */
+    protected function calculateAmount()
+    {
+        wc_load_cart();
+        $amount = WC()->cart->get_total('edit');
+        if (is_product() && is_numeric(wc_get_product()->get_price('edit'))) {
+            return $amount + (float)wc_get_product()->get_price('edit');
+        }
+
+        return $amount;
+    }
+
+    /**
+     * Adds action to place the banner on desired location
+     */
     protected function placeBannerOnPage()
     {
         $hook = $this->hookForCurrentPage();
@@ -185,6 +262,7 @@ class PayPalBannerAssetManager
                 ?>
                 <div id="paypal-credit-banner"></div>
                 <?php
+
             }
         );
         if (is_home()) {
@@ -200,6 +278,9 @@ class PayPalBannerAssetManager
         }
     }
 
+    /**
+     * @return string The hook to use to place the banner
+     */
     protected function hookForCurrentPage()
     {
         if (is_cart()) {
@@ -214,17 +295,5 @@ class PayPalBannerAssetManager
         if (is_shop() || is_category() || is_search()) {
             return 'woocommerce_before_shop_loop';
         }
-    }
-
-    /**
-     * @param $option
-     *
-     * @return bool
-     */
-    protected function isEnabledShowBannerInPage($option)
-    {
-        return wc_string_to_bool(
-            get_option($option, 'no')
-        );
     }
 }
