4b294ef4752dcc51b019caf78b84a6a3e8b2f8af
carmenmaymo <mayen22@gmail.com>
2020-04-01 09:19:35 +0200

    Add default setting values in script
    PPP-412
    Also corrected the default options to send to text layout.

--- resources/js/paypalBanner.js
+++ resources/js/paypalBanner.js
@@ -9,25 +9,40 @@
 
   window.addEventListener('load', () => {
     const settings = paypalBannerFrontData.settings
-    let options = {
-      amount: settings.amount,
-      style: {
-        layout: settings.style.layout,
-        color: settings.style.color,
-        ratio: settings.style.ratio
+    if (!settings.style) {
+      settings['style'] = {
+        layout: 'text',
+        color: 'blue',
+        ratio: '8x1',
+        logo: { type: 'primary', color: 'black' }
       }
     }
-    if (settings && settings.style.layout !== 'flex') {
+    if (!settings.style.logo) {
+      settings.style['logo'] = {
+        type: 'primary',
+        color: 'black'
+      }
+    }
+
+    let options = {
+      amount: settings.amount || 0,
+      style: {
+        layout: settings.style.layout || 'text',
+        logo: {
+          type: settings.style.logo.type
+        },
+        text: {
+          color: settings.style.logo.color
+        }
+      }
+    }
+    if (settings && settings.style.layout !== 'text') {
       options = {
-        amount: settings.amount,
+        amount: settings.amount || 0,
         style: {
-          layout: settings.style.layout,
-          logo: {
-            type: settings.style.logo.type
-          },
-          text: {
-            color: settings.style.logo.color
-          }
+          layout: settings.style.layout || 'flex',
+          color: settings.style.color || 'blue',
+          ratio: settings.style.ratio || '8x1'
         }
       }
     }
